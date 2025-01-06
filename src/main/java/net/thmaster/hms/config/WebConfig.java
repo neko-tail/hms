61710479626ea8ef94926acbf9c788d400e4aa89
master <2711473070@qq.com>
2025-01-06 20:11:26 +0800

    cors

---
+++ src/main/java/net/thmaster/hms/config/WebConfig.java
@@ -0,0 +1,24 @@
+package net.thmaster.hms.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author master
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
+
